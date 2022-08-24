bc2c8070112271e36c96b61e8b35779f1c03e364
clmntgbr <clement.goubier@gmail.com>
2022-08-24 22:46:31 +0200

    Adding design for all bottles

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wine_app/model/get_cellars.dart';
 import 'package:wine_app/api_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,22 +27,340 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: const AppBarWidget(),
-        body: Container(
-          color: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: const [
+              SearchSection(),
+              BottlesSection(),
+            ],
+          ),
         ));
   }
 }
 
-class CellarPage extends StatelessWidget {
-  const CellarPage({Key? key, required Future<Cellars?> cellars})
+class SearchSection extends StatelessWidget {
+  const SearchSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Saint-Emilion Grand Cru',
+                      contentPadding: EdgeInsets.all(10),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(10),
+                    shape: const CircleBorder(),
+                    shadowColor: Colors.white,
+                  ),
+                  child: const Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 26,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottlesSection extends StatelessWidget {
+  const BottlesSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final List hotelList = [
+      {
+        'title': 'Grand Royl Hotel',
+        'place': 'wembley, London',
+        'distance': 2,
+        'review': 36,
+        'picture': 'assets/images/hotel_1.png',
+        'price': '180',
+      },
+      {
+        'title': 'Queen Hotel',
+        'place': 'wembley, London',
+        'distance': 3,
+        'review': 13,
+        'picture': 'assets/images/hotel_2.png',
+        'price': '220',
+      },
+      {
+        'title': 'Grand Mal Hotel',
+        'place': 'wembley, London',
+        'distance': 6,
+        'review': 88,
+        'picture': 'assets/images/hotel_3.png',
+        'price': '400',
+      },
+      {
+        'title': 'Hilton',
+        'place': 'wembley, London',
+        'distance': 11,
+        'review': 34,
+        'picture': 'assets/images/hotel_4.png',
+        'price': '910',
+      },
+    ];
+
+    return Container(
+      padding: const EdgeInsets.all(10),
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '550 bottles founds',
+                  style: GoogleFonts.nunito(
+                    color: Colors.black,
+                    fontSize: 15,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Filters',
+                      style: GoogleFonts.nunito(
+                        color: Colors.black,
+                        fontSize: 15,
+                      ),
+                    ),
+                    const IconButton(
+                      icon: Icon(
+                        Icons.filter_list_outlined,
+                        size: 25,
+                      ),
+                      onPressed: null,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Column(
+            children: hotelList.map((hotelData) {
+              return BottlesCard(hotelData: hotelData);
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottlesCard extends StatelessWidget {
+  final Map hotelData;
+
+  const BottlesCard({Key? key, required this.hotelData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      height: 230,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(18),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade200,
+            spreadRadius: 4,
+            blurRadius: 6,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Container(
+            height: 140,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(18),
+                topRight: Radius.circular(18),
+              ),
+              image: DecorationImage(
+                image: AssetImage(
+                  hotelData['picture'],
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 5,
+                  right: -15,
+                  child: MaterialButton(
+                    color: Colors.white,
+                    shape: const CircleBorder(),
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.favorite_outline_rounded,
+                      size: 20,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  hotelData['title'],
+                  style: GoogleFonts.nunito(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                Text(
+                  '\$' + hotelData['price'],
+                  style: GoogleFonts.nunito(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  hotelData['place'],
+                  style: GoogleFonts.nunito(
+                    fontSize: 14,
+                    color: Colors.grey[500],
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Row(
+                  children: [
+                    const Icon(
+                      Icons.place,
+                      size: 14.0,
+                    ),
+                    Text(
+                      '${hotelData['distance']} km to city',
+                      style: GoogleFonts.nunito(
+                        fontSize: 14,
+                        color: Colors.grey[500],
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'per night',
+                  style: GoogleFonts.nunito(
+                    fontSize: 14,
+                    color: Colors.grey.shade800,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 3, 10, 0),
+            child: Row(
+              children: [
+                Row(
+                  children: const [
+                    Icon(
+                      Icons.star_rate,
+                      size: 14.0,
+                    ),
+                    Icon(
+                      Icons.star_rate,
+                      size: 14.0,
+                    ),
+                    Icon(
+                      Icons.star_rate,
+                      size: 14.0,
+                    ),
+                    Icon(
+                      Icons.star_rate,
+                      size: 14.0,
+                    ),
+                    Icon(
+                      Icons.star_border,
+                      size: 14.0,
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 20),
+                Text(
+                  '${hotelData['review']} reviews',
+                  style: GoogleFonts.nunito(
+                    fontSize: 14,
+                    color: Colors.grey[500],
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CellarsPage extends StatelessWidget {
+  const CellarsPage({Key? key, required Future<Cellars?> cellars})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          color: Colors.white,
-        ),
+        body: const CellarsSection(),
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(50.0),
           child: AppBar(
@@ -62,6 +381,17 @@ class CellarPage extends StatelessWidget {
   }
 }
 
+class CellarsSection extends StatelessWidget {
+  const CellarsSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
+
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   const AppBarWidget({Key? key}) : super(key: key);
 
@@ -74,17 +404,25 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Colors.white,
       actions: [
         IconButton(
+          icon: Icon(
+            Icons.favorite_outline_rounded,
+            color: Colors.grey[800],
+            size: 20,
+          ),
+          onPressed: null,
+        ),
+        IconButton(
             onPressed: () {
               Navigator.push(
                   context,
                   MaterialPageRoute(
                       builder: (_) =>
-                          CellarPage(cellars: ApiService().getCellars())));
+                          CellarsPage(cellars: ApiService().getCellars())));
             },
-            icon: const Icon(
+            icon: Icon(
               Icons.app_registration_rounded,
-              color: Colors.black,
-              size: 30,
+              color: Colors.grey[800],
+              size: 20,
             ))
       ],
     );
