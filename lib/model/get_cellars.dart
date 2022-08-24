829f2a99451e5f8456f61839faf2b019098f8048
clmntgbr <clement.goubier@gmail.com>
2022-08-24 15:09:01 +0200

    removing unusued import

--- lib/model/get_cellars.dart
+++ lib/model/get_cellars.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 Cellars cellarsFromJson(String str) => Cellars.fromJson(json.decode(str));
